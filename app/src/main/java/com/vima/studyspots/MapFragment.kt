69188ecbc316951a99ccf9b4fb0de48154f3a1e9
melodyyin <melody.yin2001@utexas.edu>
2022-10-25 22:23:55 -0500

    Melody: Created nav graph, trying to navigate to list using button on map view, currently crashing

--- app/src/main/java/com/vima/studyspots/MapFragment.kt
+++ app/src/main/java/com/vima/studyspots/MapFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +21,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class MapFragment : Fragment() {
 
+    //var navToList: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,26 +32,13 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
+        val navToList = view.findViewById<Button>(R.id.listButton)
+        navToList?.setOnClickListener {
+            //val text = view.findViewById<TextView>(R.id.mapText)
+            //text.text = "test"
+            Navigation.findNavController(view).navigate(R.id.action_mapFragment_to_studyListFragment)
+        }
+        return view
     }
-
-    /*companion object {
-        *//**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment mapFragment.
-         *//*
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            mapFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }*/
 }
