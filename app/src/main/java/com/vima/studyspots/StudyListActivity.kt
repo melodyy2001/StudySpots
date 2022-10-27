68bc708e04d640f3e65d1825dae87fcde1829a55
melodyyin <melody.yin2001@utexas.edu>
2022-10-27 13:56:03 -0500

    Melody: Added Recycler view to list xml and setup kotlin file

--- app/src/main/java/com/vima/studyspots/StudyListActivity.kt
+++ app/src/main/java/com/vima/studyspots/StudyListActivity.kt
@@ -2,11 +2,19 @@ package com.vima.studyspots
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.vima.studyspots.adapters.StudySpotAdapter
 
 class StudyListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_study_list)
+
+
+        val recyclerView = findViewById<RecyclerView>(R.id.grid_recycler_view)
+        recyclerView.adapter = StudySpotAdapter(applicationContext)
+
+        recyclerView.setHasFixedSize(true)
     }
 
 
