b025e5472a97008c9ef73efd0932486bf23b566c
melodyyin <melody.yin2001@utexas.edu>
2022-11-10 20:25:07 -0600

    Melody: Changes to profile xml

--- app/src/main/java/com/vima/studyspots/data/DataSource.kt
+++ app/src/main/java/com/vima/studyspots/data/DataSource.kt
@@ -12,8 +12,8 @@ object DataSource {
             R.drawable.welch,
             "Robert A. Welch Hall (WEL)",
             "105 E 24th St, Austin, TX 78705",
-            "Located across from the Gates-Dell Complex and at the heart of campus, Welch Hall" +
-                    "is the newly renovated hub for the Chemistry Department. It features some of the largest" +
+            "Located across from the Gates-Dell Complex and at the heart of campus, Welch Hall " +
+                    "is the newly renovated hub for the Chemistry Department. It features some of the largest " +
                     "lecture halls on campus and a variety of public study spaces.",
             mutableListOf(StudyRoom("Welch Lobby", "3/5", "3.5/5",
                 "Area gets busy in between classes", "4/5"))
@@ -22,7 +22,7 @@ object DataSource {
             R.drawable.eer,
             "Engineering Education and Research Center (EER)",
             "2501 Speedway, Austin, TX 78712",
-            "The EER is located at the Northern tip of campus where Dean Keeton and Speedway" +
+            "The EER is located at the Northern tip of campus where Dean Keeton and Speedway " +
                     "intersect. The first and second floors make up a huge, open lobby area where students" +
                     " from all different majors can be found studying.",
             mutableListOf(StudyRoom("EER Lobby", "2/5", "3/5",
@@ -32,9 +32,9 @@ object DataSource {
             R.drawable.energy,
             "Gary L. Thomas Energy Engineering Building (GLT)",
             "210 E 24th St, Austin, TX 78712",
-            "Located just steps away from the EER, the energy building is the newest edition to the" +
+            "Located just steps away from the EER, the energy building is the newest edition to the " +
                     "engineering quad, opening to the public in the Fall of 2022. There are " +
-                    "conference rooms sprinkled across its 184,000 square feet of space and a lounge with" +
+                    "conference rooms sprinkled across its 184,000 square feet of space and a lounge with " +
                     "180 degree windows that feature a stunning view of campus.",
             mutableListOf(StudyRoom("TODO", "2/5", "3/5",
                 "TODO.", "2.5/5"))
@@ -44,7 +44,7 @@ object DataSource {
             "Gates Dell Complex (GDC)",
             "2317 SPEEDWAY, AUSTIN, TX 78712",
             "The Gates-Dell Complex can be found at the center of Speedway, located across from Welch and " +
-                    "facing towards the UT Tower. There are seven floors in the GDC with public study areas and a balcony" +
+                    "facing towards the UT Tower. There are seven floors in the GDC with public study areas and a balcony " +
                     "on each of floors 3-6, as well as a computer lab and a back patio.",
                     mutableListOf(StudyRoom("GDC Lobby", "2/5", "2/5",
                 "Location of newly opened Lucky Lab.", "2.5/5"))
@@ -66,7 +66,7 @@ object DataSource {
         "Perry-Castañeda Library (PCL)",
         "101 E. 21st Street\n" +
                 "Austin, Texas 78712",
-        "The PCL is the largest on-campus library, located at the Southern tip of Speedway and featuring" +
+        "The PCL is the largest on-campus library, located at the Southern tip of Speedway and featuring " +
                 "six floors, each with its own personality and set of unique study areas.",
         mutableListOf(StudyRoom("Learning labs", "4/5", "4/5",
             "Tucked behind the elevators on the entry level of the PCL", "4/5"),
